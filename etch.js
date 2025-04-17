840cc191f659c72d084d72058e9e6280c56115fa
Mikhail <mitenber@gmail.com>
2025-04-17 13:58:42 -0400

    few changes to styles and js file before leaving project for now

---
+++ etch.js
@@ -0,0 +1,7 @@
+function makeGrid(size){
+    let screen = document.querySelector(".screen");
+    for(let i = 0; i < size; i++) {
+        let column = document.createElement("div");
+        column.classList
+    }
+}
