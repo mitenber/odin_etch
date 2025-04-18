777653f5c95aaaee5189e5dc1d33b009a012e956
Mikhail <mitenber@gmail.com>
2025-04-18 12:06:20 -0400

    got the grid working

--- etch.js
+++ etch.js
@@ -1,7 +1,34 @@
-function makeGrid(size){
-    let screen = document.querySelector(".screen");
-    for(let i = 0; i < size; i++) {
-        let column = document.createElement("div");
-        column.classList
+let screen = document.getElementById("board");
+let row;
+let column;
+let GRID_SIZE = 16;
+
+/*for (let i = 0; i < GRID_SIZE; i++) {
+    row = document.createElement("div");
+    row.className = "flex-container";
+    row.innerHTML = "";
+    screen.appendChild(row);
+
+    for (let j = 0; j < GRID_SIZE; j++) {
+        column = document.createElement("div");
+        column.className = "flex-boxes";
+        column.classList.add = "rows";
+        column.innerHTML = 'a';
+        row.appendChild(column);
+    }
+
+}*/
+
+for (i=0; i<GRID_SIZE; i++){
+    row = document.createElement("div");
+    row.className = 'rows';
+    
+    screen.appendChild(row);
+    for (j=0; j<GRID_SIZE; j++) {
+        column = document.createElement("div");
+        column.className = 'columns';
+        column.innerHTML = i+j;
+        row.appendChild(column);
     }
 }
+    
